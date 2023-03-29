c499ebb13200df491d063acaddf29f2b4a977b34
eJwtzMGKwjAUheF3ybqLapoasx2YhSjOYhhhNuG2uamhaRNuWquI726Q7g4Hvv/JoG0xpd/Q48gUK0tRVwL2ssKaY7WzlgsJxkjLK8st1rJsNrbcsYJFCjdnkFb6XEt6WlPdNeivbrrDfDp/X+SJ4+G2FcvxKsSfkIf/5rK1w/hzzKkP0dMjYnYNAiHlN7Xhc4AZ3Kji3HjX6h4fBSEYFajLI4ZiTkgKB3C+WAL11oclY7xHR5g0TEyNs/ev1xshNk+ <mhjony336@gmail.com>
2023-03-29 22:47:17 +0600

    First Setup project

---
+++ cinephiles/resources/views/starter.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+   <!-- Required meta tags -->
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Cinephiles</title>
+   <!-- Favicon -->
+   <!-- <link rel="shortcut icon" href="images/favicon.ico"/> -->
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="css/bootstrap.min.css"/>
+   <!-- Typography CSS -->
+   <link rel="stylesheet" href="css/typography.css">
+   <!-- Style -->
+   <link rel="stylesheet" href="css/style.css"/>
+   <!-- Responsive -->
+   <link rel="stylesheet" href="css/responsive.css"/>
+</head>
+
